31bde8afe12616bcdf0ca2d1a5646e3626404122
Kody Barton <kbarton9@gatech.edu>
2023-10-15 00:59:03 -0700

    TODO: reimplement flyto functionality

--- Trip.py
+++ Trip.py
@@ -11,15 +11,7 @@ class Trip(object):
         self.fname = fname
         self.gpx = gpxpy.parse(gpx_file)
         self.trip_data = self.set_trip_data()
-        self.polyline = self.set_polyline()
 
-    def set_polyline(self):
-        """Returns a leaflet PolyLine Object corresponding to the trip
-
-        Returns:
-            PolyLine: Leaflet Polyline object for the trip
-        """
-        return folium.PolyLine(self.trip_data[['latitude', 'longitude']], color='green', weight=4.5, opacity=.5)
 
     def set_trip_data(self):
         """Parses gpx data and inputs into dataframe with latitude, longitude, elevation, and time columns
@@ -34,18 +26,9 @@ class Trip(object):
                     trip_data = trip_data._append({'latitude': point.latitude,
                                                    'longitude': point.longitude,
                                                    'elevation': point.elevation,
-                                                   'time': point.time}, ignore_index=True)
+                                                   'latlng': [point.latitude, point.longitude]}
+                                                   , ignore_index=True)
         return trip_data
-
-    def plot_trip(self):
-        """Plots trip on a leaflet map and saves to HTML file in current directory (trip_map.html)"""
-        trip_map = folium.Map(location=(self.trip_data['latitude'].mean(), self.trip_data['longitude'].mean()),
-                              tiles="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
-                              attr='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> '
-                                   'contributors &copy;<a href="https://carto.com/attributions">CARTO</a>',
-                              )
-        self.polyline.add_to(trip_map)
-        trip_map.save("trip_map.html")
 
     def get_mean_latitude(self):
         """Calculates the mean latitude of the trip data
@@ -61,18 +44,22 @@ class Trip(object):
             float: average longitude"""
         return self.trip_data['longitude'].mean()
 
-    def upload_to_db(self):
+    def upload_to_db(self, verbose=True):
         """Uploads high-level trip data to trips.db SQL database."""
         con = sqlite3.connect("trips.db")
         cur = con.cursor()
+        if verbose==True:
+            print('Adding gpx data to database: ' + self.fname)
         # Check if trip already exists in db, and return if so
         res = cur.execute("""SELECT * FROM trips WHERE fname={fname}""".format(fname="'"+self.fname+"'"))
         if res.fetchone() is not None:
             return
         cur.execute("""
-            INSERT INTO trips(leaflet_id, fname, latitude_centroid, longitude_centroid) VALUES
-                ({id}, {fname}, {lat}, {long})
-        """.format(id="'"+self.polyline.get_name()+"'", fname="'"+self.fname+"'", lat=self.get_mean_latitude(),
-                   long=self.get_mean_longitude()))
+            INSERT INTO trips(fname, name, latitude_centroid, longitude_centroid, latlng) VALUES
+                ({fname}, {name},{lat}, {long}, {latlng})
+        """.format(fname="'"+self.fname+"'",
+                   name="'"+self.fname.split('.gpx')[0].split('/')[1].replace('_',' ')+"'", lat=self.get_mean_latitude(),
+                   long=self.get_mean_longitude(),
+                   latlng="'"+str(self.trip_data['latlng'].tolist())+"'"))
         con.commit()
         con.close()
--- test.py
+++ test.py
@@ -1,13 +1,9 @@
 from Trip import Trip
-from TripBook import TripBook
 import os
 
 if __name__ == '__main__':
-    kodys_trips = TripBook("Kody's Trips")
     for filename in os.scandir('data'):
         if filename.is_file() and filename.path.endswith('.gpx'):
             gpx_file = open(filename.path, 'r')
             temp = Trip(gpx_file, filename.path.replace(' ', '_').replace('\'', ''))
-            kodys_trips.add_trip(temp)
             temp.upload_to_db()
-    kodys_trips.plot_trips()
